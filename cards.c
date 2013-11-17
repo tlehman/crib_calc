79652c47d393a40a6f16bb2ea6eea8abf4580b14
Tobi Lehman <tobi.lehman@gmail.com>
2013-11-17 14:04:24 -0800

    avoid unnecessary use of strcmp

--- cards.c
+++ cards.c
@@ -25,10 +25,8 @@ const char *suit_to_s(Suit s)
 const char *rank_to_s(Rank r)
 {
     unsigned i = (unsigned)r;
-    const char *ranks[] = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
-
     if(i >= 13) return "\0";
-
+    const char *ranks[] = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
     return ranks[i];
 }
 
@@ -37,7 +35,7 @@ Rank s_to_rank(const char s[])
     unsigned int i = 0;
     const char *ranks[] = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
     for(i = 0; i < sizeof(ranks); ++i) {
-	if(0 == strcmp(s, ranks[i])) return (Rank)i;
+        if(s[0] == ranks[i][0]) return (Rank)i;
     }
     printf("error: '%s' is not a rank", s);
     exit(-1);
@@ -48,23 +46,23 @@ Suit s_to_suit(const char s[])
     switch(s[0]) {
     case 'c':
     case 'C':
-	return club;
-	break;
+        return club;
+        break;
     case 'h':
     case 'H':
-	return heart;
-	break;
+        return heart;
+        break;
     case 's':
     case 'S':
-	return spade;
-	break;
+        return spade;
+        break;
     case 'd':
     case 'D':
-	return diamond;
-	break;
+        return diamond;
+        break;
     default:
-	printf("error: '%s' is not a suit", s);
-	exit(-1);
+        printf("error: '%s' is not a suit", s);
+        exit(-1);
     }
 } 
 
@@ -87,9 +85,9 @@ void init_card_from_string(Card *c, const char *str)
     char pattern[] = "([A2-9JKQ]|10)([CHSD])";
     regex_t reg;
 
-    int max = 3; // the max length is 4 == strlen("10C")+1 (the null char)
-    char rank_s[max];
-    char suit_s[max];
+    int max_len = 6; // the max length is 4 == strlen("10C")+1 (the null char)
+    char rank_s[max_len];
+    char suit_s[max_len];
 
     /* since there are only two pieces to be extracted from the string,
        the rank and the suit, the number of matches should be two, plus 
@@ -99,8 +97,8 @@ void init_card_from_string(Card *c, const char *str)
 
     // use extended regexes, case insensitive
     if(0 != regcomp(&reg, pattern, REG_EXTENDED | REG_ICASE)) {
-	printf("error compiling regex /%s/", pattern);
-	exit(-1);
+        printf("error compiling regex /%s/", pattern);
+        exit(-1);
     }
 
     regexec(&reg, str, n, matches, 0);
--- hand.c
+++ hand.c
@@ -3,14 +3,14 @@
 int main(int argc, char *argv[])
 {
     int i = 1;
-    int count = argc - 1;
-    Card hand[count];
+    Card hand[argc];
 
-    draw_random_cards(hand, count);
+    draw_random_cards(hand, argc);
 
-    for(i = 1; i < count; ++i) {
-	init_card_from_string(&hand[i], argv[i]);
-        print_card(hand[i]);
+    for(i = 1; i < argc; ++i) {
+        printf("%d: %s\n", i, argv[i]);
+        init_card_from_string(&hand[i], argv[i]);
+        //print_card(hand[i]);
     }
 
     printf("\n");
