840b9f111819a08a851ec484828f872a8f7d67eb
Tobi Lehman <tobi.lehman@gmail.com>
2013-12-08 17:32:42 -0800

    finish documenting flush count

--- src/cribbage.c
+++ src/cribbage.c
@@ -78,20 +78,51 @@ int score_and_print(Card *hand, int count)
     //    A Run is a sequence of 3 or more cards with contiguous ranks.
     //
     //    To find runs, we keep track of these data:
-    //      run length    int
+    //      run_length    int
     //      multiplicity  int
     //      index         int
     //      rank_map      int[13]
     //
     //    Before we introduce the algorithm, it is helpful to see an 
-    //    example, suppose 
+    //    example, suppose our hand is:
     //        9♥  8♥  6♣  7♠  8♦  8♣
+    //    we start by computing rank_map: 
+    //        A 2 3 4 5 6 7 8 9 10 J K Q
+    //        0 0 0 0 0 1 1 3 1  0 0 0 0
+    //                  ^ scan until the first nonzero value is found, 
+    //                    then scan until you hit the next zero value:
+    //        A 2 3 4 5 6 7 8 9 10 J K Q
+    //        0 0 0 0 0 1 1 3 1  0 0 0 0
+    //                           ^ at this point you should have run_length = 4
+    //                             and 4 >= 3, so it counts as a run.
+    //                             Notice that you have three runs:
+    //                                  6♣  7♠  8♦  9♥  
+    //                                   "   "  8♥   "
+    //                                   "   "  8♣   "
+    //                             This is why multiplicity is multiplied 
+    //                             with run_length to compute the score.
     //
     //    The algorithm goes like this:
     //      Initialize index        = 0
     //                 multiplicity = 1
-    //                 run_length   = 
-
+    //                 run_length   = 0
+    //      Scan from left to right (increasing index)
+    //
+    //      Loop index 0 to 13
+    //          if rank_map[index] > 0
+    //              run_length += 1
+    //              multiplicity *= rank_map[index]
+    //          else
+    //              if run_length >= 3
+    //                  (size = run_length, 
+    //                   score = multiplicity * run_length)
+    //              endif
+    //              run_length = 0
+    //              multiplicity = 1
+    //          endif
+    //      End Loop
+    for(i = 0; i < 13; ++i) {
+    }
 
     // Fifteens
 
